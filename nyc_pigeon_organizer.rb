17a0ae5b241fb80fa60744e6c9e1876ac2a000e4
Aiden Seitz <csa.aiden@gmail.com>
2021-03-04 02:24:23 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
-def nyc_pigeon_organizer(data)
-  # write your code here!
+
+def nyc_pigeon_organizer(pigeon_data)
+
+  results = pigeon_data.each_with_object({}) do |(key, values), final_array|
+  # binding.pry
+    values.each do |inner_key, names|
+    # binding.pry
+      names.each do |name|
+      # binding.pry
+        if !final_array[name] #If there is no "Theo"name as a key in {final array}
+          final_array[name] = {}
+        end
+          if !final_array[name][key]
+            final_array[name][key] = []
+          end
+          final_array[name][key] << inner_key
+      end
+    end  
+  end
 end
+
